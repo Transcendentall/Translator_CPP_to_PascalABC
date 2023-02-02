678807e49244fd2cdc2798ff24cd19b2f742e475
Transcendentall <60776766+Transcendentall@users.noreply.github.com>
2023-02-02 18:50:45 +1000

    Add files via upload

--- Translator_CPP_to_PascalABC/source/Input_Code_CPP.cpp
+++ Translator_CPP_to_PascalABC/source/Input_Code_CPP.cpp
@@ -1,12 +1,45 @@
-int main()
-{
-    int a = 3;
-    int c;
-    if (a == 3) {
-        c = 10;
+int super_calculator(int n) {
+    int b = n + 2;
+    int calc = 0;
+    b = b * 2;
+    if (b < 10)
+    {
+        int u = 3;
+        calc = u;
+        calc = b - 4;
     }
     else
     {
-        c = 15;
+        if (b > 20)
+        {
+            int u = 6;
+            calc = b * b;
+        }
+        else
+        {
+            std::cout << 00000000000000000000;
+            calc = 2;
+            if (b == 15)
+            {
+                int u = 9;
+                while (b > 12)
+                {
+                    calc++;
+                    calc = calc + u;
+                    b--;
+                }
+            }
+        }
+    }
+    return calc;
+}
+
+void main() {
+    int a = 3;
+    if (a == 3)
+    {
+        int r = 5;
+        r = super_calculator(a);
+        std::cout << r;
     }
 }
